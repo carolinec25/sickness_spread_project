2f0ad83ff97ac6014385d5df3ad8841407b2ff9f
cpuGuardian <tayo@thefooses.com>
2026-03-01 13:46:08 -0500

    spoke model made

--- main.py
+++ main.py
@@ -47,22 +47,24 @@ class Hallway():
     def gen_walls(self):
         
         def randomRoom(baseX,baseY,baseW,baseH,
-                       randX=1/10,randY=1/10,randW=3/20,randH=3/20,color = "black"):
+                       randX=1/10,randY=1/10,randW=3/20,randH=3/20,color = "black",rand=True):
             #initialize to base value
             x = round(baseX*self.width)
             y = round(baseY*self.height)
 
-            #add/subtract random amounts
-            x+= random.randint(round(-randX*self.width), round(randY*self.width))
-            y+= random.randint(round(-randX*self.height), round(randY*self.height))
+            if (rand):
+                #add/subtract random amounts
+                x+= random.randint(round(-randX*self.width), round(randY*self.width))
+                y+= random.randint(round(-randX*self.height), round(randY*self.height))
 
             #initialize to base value
             w = round(x+ baseW*self.width)
             h = round(y+ baseH*self.height)
 
-            #add/subtract random amounts
-            w+= random.randint(0,round(randW*self.width))
-            h+= random.randint(0,round(randH*self.height))
+            if (rand):
+                #add/subtract random amounts
+                w+= random.randint(0,round(randW*self.width))
+                h+= random.randint(0,round(randH*self.height))
 
             self.canvas.create_rectangle(x, y, w, h,fill=color)
             for i in range(x, min(x, self.width)):
@@ -70,22 +72,25 @@ class Hallway():
                     self.walls[i][j] = True
         
         def centerRoom(midX,midY,baseW,baseH,
-                       randX=1/10,randY=1/10,randW=3/20,randH=3/20,color = "black"):
+                       randX=1/10,randY=1/10,randW=3/20,randH=3/20,color = "black",
+                       rand = True):
             #initialize to base value
             x = round(midX*self.width-.5*baseW*self.height)
             y = round(midY*self.height-.5*baseH*self.height)
 
-            #add/subtract random amounts
-            x+= random.randint(round(-randX*self.width), round(randY*self.width))
-            y+= random.randint(round(-randX*self.height), round(randY*self.height))
+            if (rand):
+                #add/subtract random amounts
+                x+= random.randint(round(-randX*self.width), round(randY*self.width))
+                y+= random.randint(round(-randX*self.height), round(randY*self.height))
 
             #initialize to base value
             w = round(x+ baseW*self.width)
             h = round(y+ baseH*self.height)
 
-            #add/subtract random amounts
-            w+= random.randint(0,round(randW*self.width))
-            h+= random.randint(0,round(randH*self.height))
+            if (rand):
+                #add/subtract random amounts
+                w+= random.randint(0,round(randW*self.width))
+                h+= random.randint(0,round(randH*self.height))
 
             self.canvas.create_rectangle(x, y, w, h,fill=color)
             for i in range(x, min(x, self.width)):
@@ -93,11 +98,16 @@ class Hallway():
                     self.walls[i][j] = True
 
         #central room
-        randomRoom(7/20,7/20,5/20,5/20)
-        #randomRoom(1/20,15/20,3/20,1/20)
-        centerRoom(1/2,1/5,1/10,15/20,randY=0,randH=0,color='blue')
-        centerRoom(1/2,1/2,2,2/20,randX=0,randW=0,color='blue')
-        centerRoom(1/2,4/5,1/10,15/20,randY=0,randH=0,color='blue')
+        centerRoom(1/2,1/2,8/20,8/20)
+        centerRoom(1/2,1/5,1/10,15/20,randY=0,randH=0,color='black')
+        centerRoom(1/2,1/2,2,2/20,randX=0,randW=0,color='black')
+        centerRoom(1/2,4/5,1/10,15/20,randY=0,randH=0,color='black')
+
+        #borders
+        randomRoom(0,0,1,2/20,rand=False)
+        randomRoom(0,0,2/20,1,rand=False)
+        randomRoom(18/20,0,2/20,1,rand=False)
+        randomRoom(0,18/20,1,2/20,rand=False)
         
 
 
