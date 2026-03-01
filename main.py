e2ac57b7ec58612bca98890f00581bad789f4026
cpuGuardian <tayo@thefooses.com>
2026-03-01 15:06:54 -0500

    FULLY fixed pixels spwning in walls

--- main.py
+++ main.py
@@ -127,14 +127,14 @@ class Hallway():
         #central room
         centerRoom(1/2,1/2,8/20,8/20)
         centerRoom(1/2,1/5,1/10,11/20,randY=0,randH=0,color='black')
-        #centerRoom(1/2,1/2,2,2/20,randX=0,randW=0,color='black')
+        centerRoom(1/2,1/2,2,2/20,randX=0,randW=0,color='black')
         centerRoom(1/2,4/5,1/10,15/20,randY=0,randH=0,color='black')
 
         
         #borders
-        #randomRoom(0,0,1,2/20,rand=False)
-        #randomRoom(0,0,2/20,1,rand=False)
-        #randomRoom(18/20,0,2/20,1,rand=False)
+        randomRoom(0,0,1,2/20,rand=False)
+        randomRoom(0,0,2/20,1,rand=False)
+        randomRoom(18/20,0,2/20,1,rand=False)
         randomRoom(0,18/20,1,2/20,rand=False)
         
         
